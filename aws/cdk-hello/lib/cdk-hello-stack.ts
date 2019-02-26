156759748a2e5e13bc0811eeee9ecf23011a7009
Pasi Niemi <pasi.niemi@nitor.com>
2019-02-26 20:49:56 +0000

    Add variables to resource names

--- aws/cdk-hello/lib/cdk-hello-stack.ts
+++ aws/cdk-hello/lib/cdk-hello-stack.ts
@@ -1,16 +1,17 @@
 import sns = require('@aws-cdk/aws-sns');
 import sqs = require('@aws-cdk/aws-sqs');
 import cdk = require('@aws-cdk/cdk');
+import vars from "../variables.json";
 
 export class CdkHelloStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkHelloQueue', {
+    const queue = new sqs.Queue(this, 'CdkHelloQueue' + vars.paramEnvId, {
       visibilityTimeoutSec: 300
     });
 
-    const topic = new sns.Topic(this, 'CdkHelloTopic');
+    const topic = new sns.Topic(this, 'CdkHelloTopic' + vars.paramEnvId);
 
     topic.subscribeQueue(queue);
   }
